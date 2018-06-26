56001a2eb35a3604a58c774b6622e639443bfd64
Omar Rasheed <omarrasheed@fb.com>
2018-06-26 13:10:28 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Omar Rasheed on 6/26/18.
+//  Copyright © 2018 Omar Rasheed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Omar Rasheed on 6/26/18.
+//  Copyright © 2018 Omar Rasheed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
